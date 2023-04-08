fceb6d075f6e6ef396605a1e1af287cc80354018
Stephen.Havig <stephen.havig@gmail.com>
2023-04-08 12:28:46 -0700

    modify  Login  v1.0

--- client/LandingPage.js
+++ client/LandingPage.js
@@ -1,11 +1,10 @@
 import React from 'react';
-// import { BrowserRouter as Router, Link } from "react-router-dom";
 import Login from './Login.js';
 import SignUp from './SignUp.js';
 
 const LandingPage = () => {
     return (
-        // <Router>
+
             <div className='landing-page'>
                 <h1>Welcome Traveler</h1>
                 <h3>Please login</h3>
@@ -15,11 +14,10 @@ const LandingPage = () => {
                 <h3>OR</h3>
                 <h4>Create an Account</h4>
                         <div id='signup-link'>
-                            testing testing PLEASE PLEEEEEEASE
-                            {/* <Link to={<SignUp />}>Create Account</Link> */}
+
                         </div>
             </div>
-            // </Router>
+
     );
 };
 
