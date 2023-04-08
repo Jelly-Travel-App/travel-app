99156771ea73fa66fdeb504b13ab96cdd1487c8f
Stephen.Havig <stephen.havig@gmail.com>
2023-04-08 12:37:05 -0700

    modify  Login  v1.0

--- client/index.js
+++ client/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
-import { createRoot } from 'react-dom/client';
-import { render } from 'react-dom'
-
-import App from './App.js';
-
-const root = createRoot(document.getElementById('root'))
-root.render(<App />);
-=======
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
->>>>>>> dev
