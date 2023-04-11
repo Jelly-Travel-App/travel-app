e51601c1f7039ad13bba5d324a898a00eeb5ba69
Eric Dunn <ericlewisdunn@gmail.com>
2023-04-11 17:45:20 -0400

    Set up testing suite  Co-authored-by: Cartlong7 <Cartlong7@users.noreply.github.com> Co-authored-by: Gary Balogh  <GaryB93@users.noreply.github.com> Co-authored-by: Suigin1 <Suigin1@users.noreply.github.com>

---
+++ tests/user.test.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const request = require("supertest");
+const app = require("../client/.App");
+
+require("dotenv").config();
+
+beforeEach(async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+  });
+
+
+//   app.post('/api/login', userController.verifyUser, (req, res) => {
+// 	console.log('login working');
+// 	res.status(200).json(res.locals.user);
+// });
+
+describe("POST /api/login")
