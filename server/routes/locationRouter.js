8705b6c3b3f0382884767a27b17f3fb59a0b7670
Cartlong7 <crlong7@gmail.com>
2023-04-12 09:06:43 -0600

    updated visual clutter of locationRouter comments, and updated location router path

--- server/routes/locationRouter.js
+++ server/routes/locationRouter.js
@@ -5,18 +5,14 @@ const locationController = require('../controllers/locationController');
 //YELP API
 // /api/location, method: POST
 locationRouter.post(
-	'/api/location/:location',
+	'/:location',
 	locationController.getRestaurants,
 	locationController.getEvents,
 	(req, res) => {
-		// console.log('location working');
-		// deconstruct the location from the request body
-		//console.log('res.locals.restaurantInfo: ', res.locals.restaurantInfo, 'res.locals.eventInfo: ', res.locals.eventInfo)
 		res.status(200).json({
 			restaurantInfo: res.locals.restaurantInfo,
 			eventInfo: res.locals.eventInfo,
 		});
-		// res.status(200).json(res.locals)
 	}
 );
 
