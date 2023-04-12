0a6c9d3a1795e360c7456d572b524c9754f52324
Gary Balogh <garybalogh93@gmail.com>
2023-04-12 15:28:19 -0500

    Removed events and notes containers

--- client/components/user/user-page.jsx
+++ client/components/user/user-page.jsx
@@ -1,9 +1,7 @@
 import React, {useState} from 'react';
 import { useLocation } from 'react-router-dom';
 import RestaurantContainer from './restaurant-container';
-import EventsContainer from './events-container';
 import NavBar from './nav-bar';
-import Notes from './notes';
 
 
 const UserPage = (props) => {
@@ -27,14 +25,10 @@ const UserPage = (props) => {
                 {/* User greeting - possibly replace with just username display */}
                 Hello {user.state.username}!!
                 <NavBar changeLocation={changeLocation}/>
-                {/* Prop drilling the location through to the restaurant and event containers */}
+                {/* Prop drilling the location through to the restaurant container */}
                 <div id='res-events-container'>
                     <RestaurantContainer location={location} />
-                    <EventsContainer location={location}/>
-                    <div>
-                        {/* Passing notes state - unclear if actually working? */}
-                        <Notes user={user.state} />
-                    </div>
+
                 </div>
             </div>
         </div>
