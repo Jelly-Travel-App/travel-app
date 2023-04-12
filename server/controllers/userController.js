28c1a322f474dad451aff05c3d991cda44d78c1a
Cartlong7 <crlong7@gmail.com>
2023-04-12 13:55:17 -0600

    finished migration of userController to async
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Cartlong7 <Cartlong7@users.noreply.github.com>
    Co-authored-by: Gary Balogh <GaryB93@users.noreply.github.com>
    Co-authored-by: Suigin1 <Suigin1@users.noreply.github.com>

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,52 +2,36 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const User = require('../models/userModel');
 
-// object for user middleware
+
 const userController = {};
 
-// create user
-userController.createUser = function (req, res, next) {
+userController.createUser = async function (req, res, next) {
 	const { username, password } = req.body;
-	// query database with username
-	user
-  .findOne({ username: username })
-  .then((data) => {
-    // if username returns a document send back an error
-    if (data) {
-      // allow the front end to check locals.user to equal false, signaling a username already exists
-      res.locals.user = false;
-      return next();
-    } else {
-      // if username does not exist in DB, create a new user doc with hashed password
-      bcrypt.hash(password, saltRounds, (err, hash) => {
-        user.create({ username: username, password: hash})
-          .then(result => {
-            console.log(`Created user: ${result}`);
-            // send back created user
-            res.locals.user = result;
-            return next();
-          })
-          .catch((error) => {
-            return next({
-              log: 'Middleware error at userController.createUser',
-              message: 'Unable to create user',
-            });
-          });
-      });
-    }
-  })
-  // error handling for querying DB
-  .catch((error) => {
+	try {
+		// query database with username
+		const user = await User.findOne({ username: username })
+		if (user) {
+		  res.locals.user = false;
+		  return next();
+		} else {
+			// if username does not exist in DB, create a new user doc with hashed password
+			const hashedPassword = await bcrypt.hash(password, saltRounds)
+			const newUser = await User.create({ username: username, password: hashedPassword})
+			console.log('Created user:', newUser);
+			// send back created user
+			res.locals.user = newUser;
+			return next();
+			};
+		} catch (error) {
     return next({
       log: 'Middleware error at userControler.createUser',
       message: 'Error querying database',
     });
-  });
+  };
 };
 
 userController.verifyUser = async function (req, res, next) {
 	const { username, password } = req.body;
-	
 	try {
 		//query database for user
 		const user = await User.findOne({ username: username })	
